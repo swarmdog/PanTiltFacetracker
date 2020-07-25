e38d882c16275ec004c81d209f5c10a2f3041396
swarmdog <47836485+swarmdog@users.noreply.github.com>
2020-07-24 21:03:46 -0500

    Fix depreciated cv2 references
    * Fix old cv2.cv.CV_ references
    * Use capture_continous and picamera module instead of cv2 read video
    * Remove LED code

--- facetracker_lbp.py
+++ facetracker_lbp.py
@@ -1,121 +1,62 @@
 #!/usr/bin/env python
-
-import cv2, sys, time, os
+from picamera.array import PiRGBArray
+from picamera import PiCamera
 from pantilthat import *
+import time
+import cv2
 
-# Load the BCM V4l2 driver for /dev/video0
-os.system('sudo modprobe bcm2835-v4l2')
-# Set the framerate ( not sure this does anything! )
-os.system('v4l2-ctl -p 8')
+# Set frame size
+FRAME_W=640
+FRAME_H=480
 
-# Frame Size. Smaller is faster, but less accurate.
-# Wide and short is better, since moving your head
-# vertically is kinda hard!
-FRAME_W = 180
-FRAME_H = 100
+# Initialize camera
+camera = PiCamera()
+camera.resolution = (FRAME_W, FRAME_H)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(FRAME_W, FRAME_H))
+time.sleep(0.1)
 
-# Default Pan/Tilt for the camera in degrees.
-# Camera range is from -90 to 90
+# Pan/tilt defaults
 cam_pan = 90
 cam_tilt = 60
+pan(cam_pan-90)
+tilt(cam_tilt-90)
 
 # Set up the CascadeClassifier for face tracking
-#cascPath = 'haarcascade_frontalface_default.xml' # sys.argv[1]
 cascPath = '/usr/share/opencv/lbpcascades/lbpcascade_frontalface.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Set up the capture with our frame size
-video_capture = cv2.VideoCapture(0)
-video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,  FRAME_W)
-video_capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, FRAME_H)
-time.sleep(2)
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        image = frame.array
+        image = cv2.flip(image, -1)
 
-# Turn the camera to the default position
-pan(cam_pan-90)
-tilt(cam_tilt-90)
-light_mode(WS2812)
+        faces = faceCascade.detectMultiScale(image, 1.1, 3, 0, (10,10))
 
-def lights(r,g,b,w):
-    for x in range(18):
-        set_pixel_rgbw(x,r if x in [3,4] else 0,g if x in [3,4] else 0,b,w if x in [0,1,6,7] else 0)
-    show()
+        for(x, y, w, h) in faces:
+                cv2.rectangle(image, (x,y), (x+w, y+h), (0,255,0), 2)
+                x = x+(w/2)
+                y = y+(h/2)
+                turn_x = float(x - (FRAME_W/2))
+                turn_y = float(y - (FRAME_H/2))
+                turn_x /= float(FRAME_W/2)
+                turn_y /= float(FRAME_H/2)
+                turn_x *= 2.5
+                turn_y *= 2.5
+                cam_pan += -turn_x
+                cam_tilt += turn_y
+                print(cam_pan-90, cam_tilt-90)
 
-lights(0,0,0,50)
+                cam_pan = max(0,min(180,cam_pan))
+                cam_tilt = max(0,min(180,cam_tilt))
+                pan(int(cam_pan-90))
+                tilt(int(cam_tilt-90))
+                break
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    # This line lets you mount the camera the "right" way up, with neopixels above
-    frame = cv2.flip(frame, -1)
-    
-    if ret == False:
-      print("Error getting image")
-      continue
+        cv2.imshow("Frame",image)
 
-    # Convert to greyscale for detection
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.equalizeHist( gray )
-
-    # Do face detection
-    faces = faceCascade.detectMultiScale(frame, 1.1, 3, 0, (10, 10))
-   
-    # Slower method 
-    '''faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=4,
-        minSize=(20, 20),
-        flags=cv2.cv.CV_HAAR_SCALE_IMAGE | cv2.cv.CV_HAAR_FIND_BIGGEST_OBJECT | cv2.cv.CV_HAAR_DO_ROUGH_SEARCH
-    )'''
-    
-    lights(50 if len(faces) == 0 else 0, 50 if len(faces) > 0 else 0,0,50)
-
-    for (x, y, w, h) in faces:
-        # Draw a green rectangle around the face
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-        # Track first face
-        
-        # Get the center of the face
-        x = x + (w/2)
-        y = y + (h/2)
-
-        # Correct relative to center of image
-        turn_x  = float(x - (FRAME_W/2))
-        turn_y  = float(y - (FRAME_H/2))
-
-        # Convert to percentage offset
-        turn_x  /= float(FRAME_W/2)
-        turn_y  /= float(FRAME_H/2)
-
-        # Scale offset to degrees
-        turn_x   *= 2.5 # VFOV
-        turn_y   *= 2.5 # HFOV
-        cam_pan  += -turn_x
-        cam_tilt += turn_y
-
-        print(cam_pan-90, cam_tilt-90)
-
-        # Clamp Pan/Tilt to 0 to 180 degrees
-        cam_pan = max(0,min(180,cam_pan))
-        cam_tilt = max(0,min(180,cam_tilt))
-
-        # Update the servos
-        pan(int(cam_pan-90))
-        tilt(int(cam_tilt-90))
-
-        break
-
-    frame = cv2.resize(frame, (540,300))
-    frame = cv2.flip(frame, 1)
-   
-    # Display the image, with rectangle
-    # on the Pi desktop 
-    cv2.imshow('Video', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+        key = cv2.waitKey(1) & 0xFF
+        rawCapture.truncate(0)
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+                break
